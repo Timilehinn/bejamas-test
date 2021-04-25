758e04e0e18da469fadd81b074bf98eb109f0f23
makinde timilehin <makindetimi@gmail.com>
2021-04-25 12:51:51 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express(); // create express app
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('build'))
 
@@ -9,6 +10,6 @@ app.get("/", (req, res) => {
 });
 
 // start express server on port 5000
-app.listen(50001, () => {
-  console.log("server started on port 5000");
+app.listen(PORT, () => {
+  console.log("server started on port ",PORT);
 });
